371184b2e0a1702c0b540e5dd96d0a92df3f4497
AMAN TYAGI <500069023@stu.upes.ac.in>
2020-09-16 21:52:07 +0530

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
-describe('Simple Math Test', () => {
+describe('Simple Add Test', () => {
  it('should return 2', () => {
         assert.equal(1 + 1, 2);
     });
  it('should return 9', () => {
         assert.equal(3 * 3, 9);
+    });
+ it('should return 1', () => {
+        assert.equal(6 * 6, 1);
     });
 });
